a783440d639fb2f0a9f2ce9bfdd3b540afac76f2
pmvroque15 <pmvroque@gmail.com>
2026-04-09 21:14:26 -0400

    Cleaned comments

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,33 +1,23 @@
 package com.pluralsight;
-
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    static void main(String[] args) {
-//        Prompt the user for the size of the sandwich (1 or 2):
-//        a. 1: Regular: base price $5.45
-//        b. 2: Large: base price $8.95
-//        Prompt the user for their age:
-//        a. Student (17 years old or younger) – receive a 10% discount
-//        b. Seniors (65 years old or older) – receive a 20% discount
-//        Display the cost of the sandwich to the screen
 
+    static void main(String[] args) {
         //Initialize Scanner
         Scanner scanner = new Scanner(System.in);
         //Create prompts
         //Declare and assign variables for inputs
-        double regularSandwichPrice = 5.45;
-        double largeSandwichPrice = 8.95;
-        double kiddieDiscount = 0.10;
-        double oldieDiscount = 0.20;
+        double regularSandwichPrice = 5.45;                 //Regular: base price $5.45
+        double largeSandwichPrice = 8.95;                   //Large: base price $8.95
+        double studentDiscount = 0.10;                       //Student (17 years old or younger) – receive a 10% discount
+        double seniorDiscount = 0.20;                        //Seniors (65 years old or older) – receive a 20% discount
         double userTotal = 0;
 
         //prompts
-        System.out.print("What size of sandwich would you like? (1 - Regular or 2 - Large) ");
+        System.out.print("What size of sandwich would you like? (1 - Regular or 2 - Large) ");              //Prompt the user for the size of the sandwich (1 or 2):
         int userSizeOfSandwich = scanner.nextInt();
-        System.out.print("How old are you? ");
+        System.out.print("How old are you? ");                                                              //Prompt the user for their age
         int userAge = scanner.nextInt();
 
         //code solution
@@ -41,19 +31,14 @@ public class Main {
         }
         //another if statement for age and discounts
         if (userAge <= 17) {
-            //print and apply 10% discount on the price
-            double kiddieDiscountAmount = userTotal * kiddieDiscount;
-            userTotal = userTotal - kiddieDiscountAmount;
+            double studentDiscountAmount = userTotal * studentDiscount;
+            userTotal = userTotal - studentDiscountAmount;
         } else if (userAge >= 65) {
-            //20% discount
-            double oldieDiscountAmount = userTotal * oldieDiscount;
-            userTotal = userTotal - oldieDiscountAmount;
+            double seniorDiscountAmount = userTotal * seniorDiscount;
+            userTotal = userTotal - seniorDiscountAmount;
         }
 
         //Display the cost of the sandwich
         System.out.printf("Your total is: $%.2f", userTotal);
-
     }
-
-
 }
